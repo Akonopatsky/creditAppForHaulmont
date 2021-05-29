a58816a5aaf1709111d55d6d0da26be561b7d61d
Akonopatsky <Akonopatsky@gmail.com>
2021-05-29 12:03:59 +0300

    blank with parent 2.4.6

---
+++ src/test/java/com/haulmont/creditProccesor/CreditAppForHaulmontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haulmont.creditProccesor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAppForHaulmontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

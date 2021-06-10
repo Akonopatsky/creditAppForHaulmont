ec023e4de02e8bf31b3d351ae3c0dbec6a2b43a7
Akonopatsky <Akonopatsky@gmail.com>
2021-06-10 12:21:10 +0300

    перенос перед созданием страницы связи банка и клиента

--- src/main/java/com/haulmont/creditProccesor/web/controllers/ClientController.java
+++ src/main/java/com/haulmont/creditProccesor/web/controllers/ClientController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -42,10 +39,16 @@ public class ClientController {
     }
 
     @PostMapping({"/client/save"})
-    public RedirectView clientSave(@ModelAttribute ClientDto newClient) {
+    public RedirectView clientSave(@ModelAttribute ClientDto newClient) throws CreditProcessorException {
         logger.info("new client {}", newClient);
         clientService.save(newClient);
         return new RedirectView("/clientService/", true);
+    }
+
+    @PutMapping({"/client/{id}/bank"})
+    public String chooseBank(Model model, @ModelAttribute ClientDto client){
+        logger.info("client {} choose a bank ", client.getId());
+        return "chooseBank.html";
     }
 
     @ModelAttribute("newClient")
